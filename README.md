bd4b8cff48abd0c3967a28ee5c6cad1956b81b61
Gabriel Nascimento <gabriell_ca@outlook.com>
2020-10-11 10:54:26 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Template React.js
+## Commands
+### Start Project
+```bash
+yarn
+```
+### Open on browser
+```bash
+yarn start
+```
+## Tree Structure
+```
+📦src
+ ┣ 📂App
+ ┃ ┣ 📂assets
+ ┃ ┃ ┣ 📂fonts
+ ┃ ┃ ┣ 📂gif
+ ┃ ┃ ┣ 📂icons
+ ┃ ┃ ┗ 📂styles
+ ┃ ┣ 📂router
+ ┃ ┃ ┗ 📜router.js
+ ┃ ┣ 📂services
+ ┃ ┣ 📂templates
+ ┃ ┣ 📜App.scss
+ ┃ ┗ 📜index.js
+ ┣ 📜index.js
+ ┗ 📜index.scss
+ ```
---
+++ src/App/router/router.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+// import login from '../templates/login/login';
+// import cadastro from '../templates/cadastro/cadastro';
+
+
+class Routes extends Component {
+  render() {
+    return (
+      // EXAMPLE
+      // <Router>
+      //   <Route exact path="/">
+      //     <Redirect to="/login" />
+      //   </Route>
+        
+      //   <Route exact path="/cadastro" component={cadastro} />
+      //   <Route exact path="/login" component={login} />
+
+      // </Router>
+      <Router>
+
+      </Router>
+    );
+  }
+}
+
+export default Routes;
